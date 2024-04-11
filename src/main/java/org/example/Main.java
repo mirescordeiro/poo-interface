7b29d7daa592bf78d38bfb5a87cc9efe563298de
Tamires Cordeiro <mirescordeiro@gmail.com>
2024-04-11 08:09:15 -0300

    feat: instancia Quadrado

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Quadrado square = new Quadrado(4);
+        square.calcArea();
+        square.printArea();
+    }
+}
