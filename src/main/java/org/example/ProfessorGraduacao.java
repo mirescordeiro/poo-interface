c9779511ed203e2ce4758a5cbed3cb74b4c81497
Tamires Cordeiro <mirescordeiro@gmail.com>
2024-04-11 09:38:04 -0300

    feat: implementa ProfessorGraduacao

---
+++ src/main/java/org/example/ProfessorGraduacao.java
@@ -0,0 +1,40 @@
+package org.example;
+
+public class ProfessorGraduacao implements Colaborador,Professor{
+    private double salario;
+    private double adicional;
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public double getAdicional() {
+        return adicional;
+    }
+
+    public ProfessorGraduacao() {
+    }
+
+    @Override
+    public void obtemTitulacao(String titulacao) {
+        if(titulacao.equalsIgnoreCase("doutorado")){
+            this.adicional = 0.5;
+        } else if (titulacao.equalsIgnoreCase("mestrado")) {
+            this.adicional = 0.2;
+        } else {
+            this.adicional = 0;
+        }
+    }
+
+    @Override
+    public void obtemSalario(int cargaHoraria, float valorBaseHora) {
+        this.salario = cargaHoraria * valorBaseHora * 4;
+        this.salario = this.salario + (this.salario * getAdicional());
+    }
+
+    @Override
+    public void printSalario() {
+        System.out.println("=====Professor Graduação=====");
+        System.out.println("salário: "+ getSalario());
+    }
+}
