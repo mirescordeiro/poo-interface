2804df90df5fae101672f8e2638a0cff9f1b242a
Tamires Cordeiro <mirescordeiro@gmail.com>
2024-04-11 08:23:53 -0300

    feat: instancia Retangulo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,24 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+        /*
+        * PARTE 1
+        * Crie um programa java que faça o cálculo da área de figuras geométricas ( quadrado, retangulo, triangulo, etc. ).
+        * Este projeto deve criar uma interface para o cálculo da área da figura geométrica e deve ter várias classes que
+        * façam o cálculo da área e que implementem a interface. Teste o programa ao instanciar um objeto da classe
+        * correspondente a cada forma geométrica e chamar o método que calcula a area
+        */
+
         Quadrado square = new Quadrado(4);
         square.calcArea();
         square.printArea();
+
+        Retangulo rectangle = new Retangulo(2, 4);
+        rectangle.calcArea();
+        rectangle.printArea();
+
+
+
+
     }
 }
