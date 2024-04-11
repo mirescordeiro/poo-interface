76ec62d3fbcad9f81c5345a3e547a1d606e780da
Tamires Cordeiro <mirescordeiro@gmail.com>
2024-04-11 08:52:28 -0300

    feat: implementa classe Estagiario

---
+++ src/main/java/org/example/Estagiario.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Estagiario implements Colaborador{
+    private double salario;
+
+    public double getSalario() {
+        return salario;
+    }
+
+    @Override
+    public void obtemSalario(int cargaHoraria, float valorBaseHora) {
+        this.salario = cargaHoraria * valorBaseHora;
+    }
+
+    @Override
+    public void printSalario() {
+        System.out.println("=====Estágiário=====");
+        System.out.println("salário: "+ getSalario());
+    }
+}
